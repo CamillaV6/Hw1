4a7b2e603a5be45ec78132a9a1ccbadbb7fceb8c
cvallett <cvallett@cs-122>
2026-09-10 16:00:37 -0400

    revisions

--- power-digit-sum.cpp
+++ power-digit-sum.cpp
@@ -2,42 +2,76 @@
 #include <iostream>
 #include <string> //std::stoi that converts to int
 #include <stdexcept> //catching exceptions
-#include <cmath> //allows for exponent calculation for testing
+#include <vector> //includes vectors
+#include <cmath> //inclues log calculation
+
+using namespace std;
 
 /*
- 
- The thought about the sum of the digits of an exponent value has 
- occured to me all the time. I can never stop thinking about the 
- sum of the digits of an exponent.
-
+ * The thought about the sum of the digits of an exponent value has 
+ * occured to me all the time. I can never stop thinking about the 
+ * sum of the digits of an exponent.
 */
 
-unsigned int power(unsigned int base, unsigned int exponent) { //calculates the exponent
+unsigned long long int power(unsigned int base, unsigned int exponent) { //calculates the exponent
 	
-	if (exponent == 0) { //exponents of 0 always equal 1
-	return 1;
-	}
+	unsigned long long int total = 1;
 	
-	unsigned int total = base;
-	
-	for (int i = 0; i < exponent - 1; i++) { //use loop so number will multiply by the base for exponent amount of times
+	for (int i = 0; i < exponent; i++) { //use loop so number will multiply by the base for exponent amount of times
 		total = total * base;
-		}
+	}
 	
 	return total;
-	} 
+} 
 	
-int sumDigits (int num) { //calculates sum of the digits
+vector<int> vectorize_digits(unsigned long long n) { //takes digits of number and puts in vector
+	
+	int num;
+	vector<int> digits;
+	
+	while (n > 0) { 
+		num = n % 10;
+		digits.insert(digits.begin(), num); //inserts at beginning for consistant num placement: 10s before 1s, 100s before 10s, etc.
+		n /= 10;
+	}
+		
+		/* 
+		 * used method from a previous java program:
+		 * loops until there are no digits in number
+		 * takes one place and adds it to sum
+		 * inserts digit
+		 * takes off one place
+		*/
+
+	return digits;
+}
+
+string vec_to_string(vector<int> vec) { //puts vector values into a string, used for debugging
+	
+	string numbers = "[";
+	
+	for (int i = 0; i < vec.size(); i++) {
+		numbers = numbers + to_string(vec[i]);
+		if (i < vec.size() - 1) { //doesn't add ", " if at end of vector
+			numbers = numbers + ", ";
+		}
+	}
+	
+	numbers = numbers + "]";
+	
+	return numbers;
+}
+	
+int sum_vector(vector<int> v) {  //takes numbers from vector and adds them
+	
 	int sum = 0;
 	
-	//looped it like this because it's how I did it in Java	
-	while (num > 0) { //lops until there are no digits in the number
-		sum += num % 10; //takes ones place and adds it to sum
-		num /= 10; //takes off digit in ones place
-		}
-	
-	return sum;
+	for (int i = 0; i < v.size(); i ++) { //loops through each number in vector
+		sum = sum + v[i];		
 	}
+
+	return sum;
+}
 
 int main(int argc, char* argv[]) { //https://www.geeksforgeeks.org/cpp/command-line-arguments-in-cpp/
 	
@@ -45,36 +79,44 @@ int main(int argc, char* argv[]) { //https://www.geeksforgeeks.org/cpp/command-l
 	int exponent;
 	
 	if (argc != 3) { //makes sure the right amoung of arguments were input
-		std::cout << "please input 2 integers" << std::endl;
+		cout << "please input 2 integers" << endl;
 		
-		return 0;
+		return 1;
 	}
 	
 	//chacking validity
 	try { //tries convering to int
-		base = std::stoi(argv[1]);
-		exponent = std::stoi(argv[2]);
+		base = stoi(argv[1]);
+		exponent = stoi(argv[2]);
 		//Google seach overview helped me converting arguments to int
 		//also helped with error types
 	}
-	catch (const std::invalid_argument& e) { //if invalid, catches and asks for int inputs
-		std::cout << "please enter integers" << std::endl;
-		return 0;
+	catch (const invalid_argument& e) { //if invalid, catches and asks for int inputs
+		cout << "please enter integers" << endl;
+		return 1;
 	}
-	catch (const std::out_of_range& e) { //if too large of a number, catches
-		std::cout << "number is too large" << std::endl;
-		return 0;
+	catch (const out_of_range& e) { //if too large of a number, catches
+		cout << "number is too large" << endl;
+		return 1;
 	}
-	if (base < 0 || exponent < 0) {
-		std::cout << "positive integers only" << std::endl;
-		return 0;
+	if (base < 0 || exponent < 0) { //checks positive int
+		cout << "positive integers only" << endl;
+		return 1;
+	}
+	if (!((exponent * log(base)) < (64 * log(2)))) {//checks overflow
+		cout << "Overflow Error! Please choose smaller numbers!" << endl;
+		return 1;
 	}
 
-	int pvalue = power(base, exponent);
-	std::cout << argv[1] << "^" << argv[2] << " = " << pvalue << std::endl;
+	unsigned long long int pvalue = power(base, exponent);
+	cout << argv[1] << "^" << argv[2] << " = " << pvalue << endl;
 	
-	int sum = sumDigits(pvalue);
-	std::cout << "sum of digits is " << sum << std::endl;
+	vector<int> digits = vectorize_digits(pvalue);
 	
-	return 0;
+	cout << vec_to_string(digits) << endl; //for testing
+	
+	int sum = sum_vector(digits);
+	cout << "sum of digits is " << sum << endl;
+		
+	return 0; //successfully ran
 }
